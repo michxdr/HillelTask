f67cf5662cbb7bfcb38f1606bf3f03dce6a9a5d3
michxdr <nikitakorpan9@gmail.com>
2025-02-02 20:09:25 +0200

    HW2

--- 1.py
+++ 1.py
@@ -1,11 +1,15 @@
-number = int(input("Enter a 4-digit number: "))
+number = int(input("Enter a 5-digit number: "))
 
-digit1 = number // 1000
-digit2 = (number // 100) % 10
-digit3 = (number // 10) % 10
-digit4 = number % 10
+digit1 = number // 10000
+digit2 = (number // 1000) % 10
+digit3 = (number // 100) % 10
+digit4 = (number // 10) % 10
+digit5 = number % 10
 
-print(digit1)
-print(digit2)
-print(digit3)
-print(digit4)
+reversed_number = (digit5 * 10000 +
+                   digit4 * 1000 +
+                   digit3 * 100 +
+                   digit2 * 10 +
+                   digit1)
+
+print(reversed_number)
