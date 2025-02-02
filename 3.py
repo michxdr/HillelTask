a0d5b2d33201e56d738ea3a7d80b1d406d60bdc7
michxdr <nikitakorpan9@gmail.com>
2025-02-02 20:37:42 +0200

    HW3

---
+++ 3.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+symbol = input("Enter symbol: ")
+
+if symbol == "+":
+    print("The sum of number: ", number1 + number2)
+elif symbol == "-":
+    print("The difference of number: ", number1 - number2)
+elif symbol == "/":
+    if number2 == 0:
+        print("Error, Division by zero is not possible.")
+    else:
+        print("The quotient of number: ", number1 / number2)
+else:
+    print("The product of number: ", number1 * number2)
