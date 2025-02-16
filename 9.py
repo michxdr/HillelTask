fe2f36fedda8283d37f80946b55add4bb6d892cf
michxdr <nikitakorpan9@gmail.com>
2025-02-16 12:49:34 +0200

    HW5.1

--- 9.py
+++ 9.py
@@ -2,10 +2,12 @@ import string, keyword
 
 name = input("Enter the variable name: ")
 
-print(
+is_valid = (
     name not in keyword.kwlist
-    and name.count('_') <= 1
     and not name[0].isdigit()
     and not any(c.isupper() for c in name)
     and all(c.isalnum() or c == '_' for c in name)
+    and "__" not in name
 )
+
+print(is_valid)
